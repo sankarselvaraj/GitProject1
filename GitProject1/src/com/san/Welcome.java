ea1d02c089a6146e7346247e3b824a1ba84de544
sankar <sankar.selvaraj@gmail.com>
2017-01-08 22:51:44 -0500

    Added Dynamic msg

--- GitProject1/src/com/san/Welcome.java
+++ GitProject1/src/com/san/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Sankar Inc");
+		System.out.println("Welcome to Sankar Inc"+args[0]);
 		System.out.println("To Git");
 		System.out.println("Done with Checin");
 	}
